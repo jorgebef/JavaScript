b8073e128975db8dd15bf784a352159d6130f991
jorgebef <jorge.befan@gmail.com>
2020-04-10 16:56:50 +0200

    Finished game with comments

--- memory-game/app.js
+++ memory-game/app.js
@@ -4,9 +4,7 @@
 
 // Upon DOM Content Loaded we will execute the code
 document.addEventListener('DOMContentLoaded', () => {
-    createBoard()
 
-})
 
 // card options
 const cardArray = [
@@ -60,23 +58,82 @@ const cardArray = [
     },
 ]
 
-// Create game board
+/*
+Below is the explanation for the randomness:
+
+The sort function checks for the first 2 values (index 0 and 1) and compares them,
+if the result is positive, they stay in the same order, if it's negative they
+switch order, then it proceeds with the following couple (index 1 and 2), etc...
+How it works is that Math.random() provides a value between 0 and 1 (excluding 1)
+so by having 0.5 - that random number (which is equally likely to be above or
+under 0.5, we have the equal probability of it being sorted or not
+*/
+cardArray.sort(() => 0.5 - Math.random())
+/*
+The code above is the arrow function version of:
+cardArray.sort(function (a, b) { return 0.5 - Math.random()})
+*/
+
+const grid = document.querySelector('.grid')
+const resultDisplay = document.querySelector('#result')
+var cardsChosen = []
+var cardsChosenId = []
+var cardsWon = []
 
 
 // Create the board
 function createBoard() {
     
-    const grid = document.querySelector('.grid')
 
-    for (var [i, card] of cardArray.entries()) {
-        var cardOnBoard = document.createElement('img')
-        cardOnBoard.setAttribute('src', 'images/blank.png')
-        cardOnBoard.setAttribute('data-id', i)
-        // cardOnBoard.addEventListener('click', flipCard)
-        grid.appendChild(cardOnBoard)
+
+    for (let i = 0; i < cardArray.length; i++) {
+        var card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        grid.appendChild(card)
+        card.addEventListener('click', flipCard) // When you click, it calls the function flipCard
     }
 }
 
-function flipCard() {
-    var cardId = this.getAttribute
+
+function checkForMatch() {
+    var cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTwoId = cardsChosenId[1]
+    if (cardsChosen[0] === cardsChosen[1] && cardsChosenId[0] != cardsChosenId[1]) {
+        alert('You found a match!!!: ' + cardsChosen[0] + cardsWon.length)
+        cards[optionOneId].setAttribute('src', 'images/white.png')
+        cards[optionTwoId].setAttribute('src', 'images/white.png')
+        cardsWon.push(cardsChosen)
+        // Update the Scoreboard
+        resultDisplay.textContent = cardsWon.length
+        if (cardsWon.length == cardArray.length/2) {
+            resultDisplay.textContent = 'Congratulations, you won!!!!!'
+        }
+    // If it's not a match, then set back to blank and try again!!!
+    } else {
+        alert('No match, try again!!')
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+    }
+    cardsChosen = []
+    cardsChosenId = []
 }
+
+
+
+function flipCard() {
+    var cardId = this.getAttribute('data-id') // You get the data-id from the card you have clicked on
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+
+    if (cardsChosen.length === 2) {
+        setTimeout(checkForMatch, 500)
+    }
+}
+
+
+createBoard()
+
+})
